8bfa0541d5c3057a4fbfbf01cdd1d6cec385a74f
Vizuetcf09 <vizuetcruz_fi@outlook.com>
2025-12-18 03:07:53 -0600

    changes in header-mobile

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
     companyName: 'WebPage',
-    webPageUrl: 'http://localhost:9600/api/products',
-    webPageApiKey: 'your_production_api_key_here'
+    webPageUrl: 'https://webpageapibackend.vercel.app/api/products',
 }
