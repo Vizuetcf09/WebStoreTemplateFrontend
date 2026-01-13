b9307680937f3d58bc63a748e045037a51235d0e
Vizuetcf09 <vizuetcruz_fi@outlook.com>
2026-01-13 03:01:12 -0600

    change in environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: false,
     companyName: 'WebPage',
-    webPageProductsLocalUrl: 'http://localhost:9600/api/products',
-    webPageProductsUrl: 'https://webpageapibackend.vercel.app/api/products',
-    webPagePayPalUrl: 'https://webpageapibackend.vercel.app/api/paypal/pay'
+    webPageProductsUrl: 'http://localhost:9600/api/products',
+    webPagePayPalUrl: 'http://localhost:9600/api/paypal'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,6 @@
 export const environment = {
     production: true,
     companyName: 'WebPage',
-    webPageProductsLocalUrl: 'http://localhost:9600/api/products',
     webPageProductsUrl: 'https://webpageapibackend.vercel.app/api/products',
     webPagePayPalUrl: 'https://webpageapibackend.vercel.app/api/paypal',
-    webPagePayPalLocalUrl: 'http://localhost:9600/api/paypal'
 }
