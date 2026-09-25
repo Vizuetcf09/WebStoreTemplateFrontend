f9e25915d1b1e21caf74b47a8e2ff15a5064de36
Vizuetcf09 <vizuetcruz_fi@outlook.com>
2026-09-24 22:20:03 -0600

    Cambios en footer

--- src/app/pages/layout/footer/footer.component.ts
+++ src/app/pages/layout/footer/footer.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'Footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './footer.component.html',
 })
 
-export class Footer { }
+export class Footer {
+  auth = inject(AuthService);
+}
